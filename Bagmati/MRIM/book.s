6e6c44957f79543354410988a0e741ef35c87efc
Dr. Bijay Lal Pradhan <35810496+bijayprad@users.noreply.github.com>
2026-01-23 19:16:28 +0545

    Create book.s

---
+++ Bagmati/MRIM/book.s
@@ -0,0 +1,20 @@
+
+1
+Anderson, D. R., Sweeney, D. J., Williams, T. A., Camm, J. D., & Cochran, J. J. (2018). Statistics for Business and Economics (13th ed.). Cengage India. 
+https://drive.google.com/file/d/1fuzK5kZjzsuG3yo0t7usPts_xzlMPrhF/view?usp=sharing
+
+2.
+Newbold, P., Carlson, W. L., & Thorne, B. (2019). Statistics for Business and Economics (9th ed.). Pearson. 
+https://drive.google.com/file/d/1k98WQVIPlRdUXjj4A-4_cpAU9mS3KSkv/view?usp=sharing
+
+
+3.
+Buglear J. (2007). Quantitative Methods for Business The A-Z of QM . Routledge 
+https://drive.google.com/file/d/1HvyHyzLWSCLWNmwdf6rWD9USX_gFa7mI/view?usp=sharing
+
+
+
+4
+Anderson, D. R., Sweeney, D. J., & Williams, T. A. (2015). An Introduction to Management Science: Quantitative Approaches to Decision Making (14th ed,). Cengage Learning. 
+https://drive.google.com/file/d/15hlrDlMNIt6qsQF-KHwQFHQziofdzFU0/view?usp=sharing
+
